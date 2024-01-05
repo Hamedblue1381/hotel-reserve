4b825aa2273916f7c080d317b44f453c12214cc6
hamedblue1381 <hamed.balanar@gmail.com>
2024-01-05 15:25:36 +0330

    feat: add new handlers, validator and err handler

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"context"
 	"hotel-project/api"
-	"hotel-project/types"
+	"hotel-project/store"
+	"hotel-project/validation"
 	"log"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.Status(fiber.StatusBadRequest).JSON(validation.GlobalErrorHandlerResp{
+			Success: false,
+			Message: err.Error(),
+		})
+	},
+}
 
 func main() {
 	err := godotenv.Load()
@@ -22,19 +33,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	db := client.Database(os.Getenv("DB_NAME"))
-	user := types.User{
-		FirstName: "test",
-		LastName:  "test",
-	}
-	api.HandlePostUser(db, user)
-	app := fiber.New()
+	coll := client.Database(os.Getenv("DB_NAME")).Collection(os.Getenv("USER_COLLECTION"))
+	userHandler := api.NewUserHandler(store.NewMongoUserStore(client, coll), validation.NewXValidator(validator.New()))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
 	apiv1.Get("/", api.HandleHomePage)
-	apiv1.Get("/users", api.HandleGetUsers)
-	apiv1.Get("/users/:id", api.HandleGetUser)
-	// apiv1.Post("/users",)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
+	apiv1.Post("/user", userHandler.HandlePostUser)
 
 	app.Listen(os.Getenv("LISTEN_ADDRESS"))
 }
